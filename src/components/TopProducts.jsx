ec71c5c7ea893314f798fc80f36e05d14d2f9f29
PabloFC <pablofc_68@hotmail.com>
2024-11-06 21:02:22 +0100

    adding data in TopProducts components

--- src/components/TopProducts.jsx
+++ src/components/TopProducts.jsx
@@ -1,6 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 const TopProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(
+          "https://fakestoreapi.com/products?limit=9"
+        );
+        const data = await response.json();
+        // console.log(data);
+        setProducts(data);
+      } catch (error) {
+        console.error("Error: ", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  const truncateString = (str, maxLength) => {
+    return str.length > maxLength ? str.substring(0, maxLength) + "..." : str;
+  };
+
   return (
     <main className="py-5">
       <div className="container">
@@ -13,7 +37,43 @@ const TopProducts = () => {
           </h2>
         </header>
       </div>
-      <div className="top_products" />
+      <div className="top_products">
+        {products.map((product) => (
+          <div className="col-12 col-sm-3" key={product.id}>
+            <div
+              className="card text-black d-flex justify-content-center align-items-center mt-2 py-2 opacidad"
+              style={{
+                border: 0,
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.05)",
+                minHeight: "300px",
+              }}
+            >
+              <i className="fab fa-apple fa-lg pt-3 pb-1 px-3"></i>
+              <img
+                className="img-fluid"
+                style={{
+                  maxHeight: "100px",
+                  maxWidth: "100px",
+                  textAlign: "center",
+                }}
+                src={product.image}
+                alt={product.title}
+              />
+              <div className="card-body">
+                <div className="text-center">
+                  <h5 className="card-title py-3">
+                    {truncateString(product.title, 24)}
+                  </h5>
+                  <p className="text-muted mb-4">{product.price} €</p>
+                  <a className="btn-primario" href="#">
+                    Add to cart
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
