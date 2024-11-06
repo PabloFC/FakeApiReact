b28bc378e3d76f6b5a508faef116eef8621c5299
PabloFC <pablofc_68@hotmail.com>
2024-11-06 21:55:34 +0100

    customizing some styles

--- src/components/TopProducts.jsx
+++ src/components/TopProducts.jsx
@@ -8,7 +8,7 @@ const TopProducts = () => {
     const fetchProducts = async () => {
       try {
         const response = await fetch(
-          "https://fakestoreapi.com/products?limit=9"
+          "https://fakestoreapi.com/products?limit=8"
         );
         const data = await response.json();
         // console.log(data);
@@ -37,24 +37,22 @@ const TopProducts = () => {
           </h2>
         </header>
       </div>
-      <div className="top_products">
+      <div className=" container top_products">
         {products.map((product) => (
           <div className="col-12 col-sm-3" key={product.id}>
             <div
-              className="card text-black d-flex justify-content-center align-items-center mt-2 py-2 opacidad"
+              className="card text-black d-flex justify-content-center align-items-center mt-2 py-2"
               style={{
                 border: 0,
                 boxShadow: "0 4px 8px rgba(0, 0, 0, 0.05)",
                 minHeight: "300px",
               }}
             >
-              <i className="fab fa-apple fa-lg pt-3 pb-1 px-3"></i>
               <img
                 className="img-fluid"
                 style={{
                   maxHeight: "100px",
                   maxWidth: "100px",
-                  textAlign: "center",
                 }}
                 src={product.image}
                 alt={product.title}
@@ -62,10 +60,12 @@ const TopProducts = () => {
               <div className="card-body">
                 <div className="text-center">
                   <h5 className="card-title py-3">
-                    {truncateString(product.title, 24)}
+                    {truncateString(product.title, 18)}
+                    {/* {product.title} */}
                   </h5>
                   <p className="text-muted mb-4">{product.price} €</p>
-                  <a className="btn-primario" href="#">
+
+                  <a className="primary_btn" href="#">
                     Add to cart
                   </a>
                 </div>
