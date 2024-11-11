392978eb0429edfaf6fe6f6b5bbaa59ce21c911e
PabloFC <pablofc_68@hotmail.com>
2024-11-11 18:38:55 +0100

    adding new routes

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React from "react";
 import Home from "../pages/Home";
 import Electronics from "../pages/Electronics";
+import Jewelry from "../pages/Jewelry";
+import Men from "../pages/Men";
+import Women from "../pages/Women";
 
 const AppRoute = () => {
   return (
@@ -10,6 +13,9 @@ const AppRoute = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/electronics" element={<Electronics />} />
+        <Route path="/jewelry" element={<Jewelry />} />
+        <Route path="/men" element={<Men />} />
+        <Route path="/women" element={<Women />} />
       </Routes>
     </BrowserRouter>
   );
