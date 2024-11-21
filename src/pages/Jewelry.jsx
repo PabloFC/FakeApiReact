7179730085022cb1aeb8210e2964dc4c8744a26f
PabloFC <pablofc_68@hotmail.com>
2024-11-21 13:16:06 +0100

    adding ProductsCard to Jewelry

--- src/pages/Jewelry.jsx
+++ src/pages/Jewelry.jsx
@@ -1,19 +1,22 @@
 import React from "react";
 import HeroCategories from "../components/HeroCategories";
 import HeaderText from "../components/HeaderText";
+import ProductsCard from "../components/ProductsCard";
+ProductsCard;
 
 const Jewelry = () => {
   return (
     <>
       <HeroCategories
         className="hero_jewelry"
-        paragraph=" find the best price!"
+        paragraph="find the best price!"
         title="Jewelry"
       />
       <HeaderText
         paragraph="Carefully created collections"
         title="Browse our Products"
       />
+      <ProductsCard />
     </>
   );
 };
