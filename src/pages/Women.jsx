576c091d29b640b309d9502c0394fd8a5a60cf6a
PabloFC <pablofc_68@hotmail.com>
2024-11-22 13:40:08 +0100

    adding ProductsCard to women page

--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeroCategories from "../components/HeroCategories";
 import HeaderText from "../components/HeaderText";
+import ProductsCard from "../components/ProductsCard";
 
 const Women = () => {
   return (
@@ -14,6 +15,7 @@ const Women = () => {
         paragraph="Carefully created collections"
         title="Browse our Products"
       />
+      <ProductsCard />
     </>
   );
 };
