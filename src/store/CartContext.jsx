05ef6267510aa9012b863b3ece0f911782bcddf6
PabloFC <pablofc_68@hotmail.com>
2024-11-30 22:21:56 +0100

    adding addToCartFunction

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,12 +1,30 @@
-import { createContext, useReducer } from "react";
-import CartReducer, { InitialCartState } from "./CartReducer";
+import { createContext, useState, useContext } from "react";
 
 export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
-  const [cart, dispatch] = useReducer(CartReducer, InitialCartState);
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const addToCart = (newProduct) => {
+    setCart((currentCart) => {
+      const existingCartItem = currentCart.find(
+        (cartItem) => cartItem.id === newProduct.id
+      );
+      if (existingCartItem) {
+        return currentCart.map((cartItem) =>
+          cartItem.id === newProduct.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      }
+      return [...currentCart, { ...newProduct, quantity: 1 }];
+    });
+    setTotal((currentTotal) => currentTotal + newProduct.price);
+  };
+
   return (
-    <CartContext.Provider value={{ cart, dispatch }}>
+    <CartContext.Provider value={{ cart, addToCart }}>
       {children}
     </CartContext.Provider>
   );
